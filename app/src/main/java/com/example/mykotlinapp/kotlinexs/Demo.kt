32c7dfac238a18aa9bd606fbe6a6b38d0132a84d
mrrr7 <mrrr7890@gmail.com>
2024-06-11 11:36:17 +0530

    higher order function

--- app/src/main/java/com/example/mykotlinapp/kotlinexs/Demo.kt
+++ app/src/main/java/com/example/mykotlinapp/kotlinexs/Demo.kt
@@ -1,5 +1,22 @@
 package com.example.mykotlinapp.kotlinexs
 
 fun main() {
-    println("hello world")
+    val upperCase1: (String) -> String = { str: String -> str.uppercase() }
+    val upperCase5: (String) -> String = { it.uppercase() }
+    println(upperCase1("hello"))
+    println(upperCase5("hello"))
+
+    val sumResult = calculate(4, 5, ::sum)                          // 4
+    val mulResult = calculate(4, 5) { a, b -> a * b }               // 5
+    println("sumResult $sumResult, mulResult $mulResult")
+
 }
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {  // 1
+    return operation(x, y)                                          // 2
+}
+
+fun sum(x: Int, y: Int) = x + y                                     // 3
+
+
+
+
