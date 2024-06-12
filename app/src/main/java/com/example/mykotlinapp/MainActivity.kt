0dfde2d7864e5d7da4775ddec4cda290e432af2c
mrrr7 <mrrr7890@gmail.com>
2024-06-12 10:45:12 +0530

    activity life cycle

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.mykotlinapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,12 +31,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clickHandler(view: View) {
-        Log.i("MainActivity-clickhandler","button clicked")
-        var dialIntent: Intent = Intent(Intent.ACTION_DIAL,Uri.parse("tel:98765432"))  //intent= intention
-        startActivity(dialIntent)
+    //chick came out of the egg --activity is visible to the user to interact - click
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "activity is started")
+    }
+
+    //sleep night -- incoming call ur activity goes into the background for sometime
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG, "activity has paused")
 
     }
+
+    //death of the chick -- activity no longer exists in the ram -- pushed to hdd[hibernate]
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "activity is stopped")
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "activity is destroyed")
+    }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickhandler", "button clicked")
+
+
+
+        fun inflateXml() {
+            var nameEditText = EditText(this)
+            nameEditText.setHint("enter ur name")
+            var pwdEditText = EditText(this)
+            pwdEditText.setHint("enter ur pass")
+            var loginButton = Button(this)
+            loginButton.setText("login")
+        }
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
+
 
 
